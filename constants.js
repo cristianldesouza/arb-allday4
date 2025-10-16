a4be7bc2ed1da057af63a86c13e2e7ed1bc1ef98
lucas <lukasg6278@gmail.com>
2025-10-16 16:06:38 -0300

    new site

---
+++ constants.js
@@ -0,0 +1,491 @@
+export default Object.freeze({
+	CACHING: true,
+	CACHE_CONTROL_TIME: 3600,
+	CMS_ENDPOINT: 'https://metacms.highstakes.tech',
+	PUBLIC_KEY:
+		'BBSIN_16XjPEEKOkcGwLHgnBFPjZgxBjFQHZ_BXn206dP3nzLpU7WryrIgmQ629AIg0rQo9I8Cl51QijEoOnsYk',
+	LANGUAGES: ['en', 'es', 'pt', 'de', 'fr'],
+	DOMAIN: 'allday4.me',
+	SITE_NAME: 'allday4',
+	SITE_SLOGAN: {
+		pt: 'Estratégia e conhecimento para transformar suas finanças',
+		es: 'Estrategia y conocimiento para transformar tus finanzas',
+		en: 'Strategy and knowledge to transform your finances',
+		de: 'Strategie und Wissen, um Ihre Finanzen zu transformieren',
+		fr: 'Stratégie et savoir pour transformer vos finances',
+	},
+	SITE_DESCRIPTION: {
+		pt: 'tipstown reúne análises, guias e conteúdos práticos que explicam o mercado de forma simples, ajudando você a investir melhor e tomar decisões financeiras com confiança.',
+		es: 'tipstown combina análisis, guías y contenido práctico para explicar el mercado de forma clara, ayudándote a invertir con inteligencia y tomar decisiones seguras.',
+		en: 'tipstown brings together analyses, guides, and practical insights that simplify the market, helping you invest smarter and make confident financial decisions.',
+		de: 'tipstown vereint Analysen, Leitfäden und praxisnahe Einblicke, die den Markt verständlich machen und Ihnen helfen, klügere und sicherere Finanzentscheidungen zu treffen.',
+		fr: 'tipstown réunit analyses, guides et conseils pratiques pour rendre le marché plus clair et vous aider à investir intelligemment et en toute confiance.',
+	},
+	AUTHORS_DESCRIPTION: {
+		pt: 'Nossa equipe é formada por analistas, economistas e especialistas em finanças comprometidos em tornar o mercado mais acessível, com análises confiáveis e linguagem simples.',
+		es: 'Nuestro equipo está compuesto por analistas, economistas y expertos financieros dedicados a hacer que el mercado sea más accesible, con análisis confiables y un lenguaje claro.',
+		en: 'Our team includes analysts, economists, and financial experts dedicated to making the market more accessible through reliable insights and clear, straightforward language.',
+		de: 'Unser Team besteht aus Analysten, Ökonomen und Finanzexperten, die sich dafür einsetzen, den Markt verständlicher zu machen – mit verlässlichen Analysen und klarer Sprache.',
+		fr: 'Notre équipe regroupe des analystes, économistes et experts financiers engagés à rendre le marché plus accessible grâce à des analyses fiables et un langage clair.',
+	},
+	MENU: {
+		pt: `<li class="menuItem">
+	 	     <a class="itemLink" href="/pt/c/criptomoedas/">
+	 	       <span class="point"></span><span class="itemTitle">Moedas Digitais</span>
+	 	     </a>
+	 	   </li>
+	 	   <li class="menuItem">
+	 	     <a class="itemLink" href="/pt/c/analise-financeira/">
+	 	       <span class="point"></span><span class="itemTitle">Alise náFinanceira</span>
+	 	     </a>
+	 	   </li>
+	 	   <li class="menuItem">
+	 	     <a class="itemLink" href="/pt/c/gestao-financeira/">
+	 	       <span class="point"></span><span class="itemTitle">Gestão de Finanças</span>
+	 	     </a>
+	 	   </li>
+	 	   <li class="menuItem">
+	 	     <a class="itemLink" href="/pt/c/investimentos/">
+	 	       <span class="point"></span><span class="itemTitle">Oportunidades de Investimento</span>
+	 	     </a>
+	 	   </li>`,
+
+		es: `<li class="menuItem">
+	 	     <a class="itemLink" href="/es/c/criptomonedas/">
+	 	       <span class="point"></span><span class="itemTitle">Monedas Digitales</span>
+	 	     </a>
+	 	   </li>
+	 	   <li class="menuItem">
+	 	     <a class="itemLink" href="/es/c/analisis-financiero/">
+	 	       <span class="point"></span><span class="itemTitle">Análisis Financiero</span>
+	 	     </a>
+	 	   </li>
+	 	   <li class="menuItem">
+	 	     <a class="itemLink" href="/es/c/gestion-financiera/">
+	 	       <span class="point"></span><span class="itemTitle">Gestión de Finanzas</span>
+	 	     </a>
+	 	   </li>
+	 	   <li class="menuItem">
+	 	     <a class="itemLink" href="/es/c/inversiones/">
+	 	       <span class="point"></span><span class="itemTitle">Oportunidades de Inversión</span>
+	 	     </a>
+	 	   </li>`,
+
+		en: `<li class="menuItem">
+	 	     <a class="itemLink" href="/c/cryptocurrencies/">
+	 	       <span class="point"></span><span class="itemTitle">Digital Currencies</span>
+	 	     </a>
+	 	   </li>
+	 	   <li class="menuItem">
+	 	     <a class="itemLink" href="/c/financial-analysis/">
+	 	       <span class="point"></span><span class="itemTitle">Financial Analysis</span>
+	 	     </a>
+	 	   </li>
+	 	   <li class="menuItem">
+	 	     <a class="itemLink" href="/c/financial-management/">
+	 	       <span class="point"></span><span class="itemTitle">Financial Management</span>
+	 	     </a>
+	 	   </li>
+	 	   <li class="menuItem">
+	 	     <a class="itemLink" href="/c/investments/">
+	 	       <span class="point"></span><span class="itemTitle">Investment Opportunities</span>
+	 	     </a>
+	 	   </li>`,
+
+		de: `<li class="menuItem">
+	 	     <a class="itemLink" href="/de/c/kryptowaehrungen/">
+	 	       <span class="point"></span><span class="itemTitle">Digitale Währungen</span>
+	 	     </a>
+	 	   </li>
+	 	   <li class="menuItem">
+	 	     <a class="itemLink" href="/de/c/finanzanalyse/">
+	 	       <span class="point"></span><span class="itemTitle">Finanzanalyse</span>
+	 	     </a>
+	 	   </li>
+	 	   <li class="menuItem">
+	 	     <a class="itemLink" href="/de/c/finanzverwaltung/">
+	 	       <span class="point"></span><span class="itemTitle">Finanzmanagement</span>
+	 	     </a>
+	 	   </li>
+	 	   <li class="menuItem">
+	 	     <a class="itemLink" href="/de/c/investitionen/">
+	 	       <span class="point"></span><span class="itemTitle">Investitionsmöglichkeiten</span>
+	 	     </a>
+	 	   </li>`,
+
+		fr: `<li class="menuItem">
+	 	     <a class="itemLink" href="/fr/c/cryptomonnaies/">
+	 	       <span class="point"></span><span class="itemTitle">Monnaies Numériques</span>
+	 	     </a>
+	 	   </li>
+	 	   <li class="menuItem">
+	 	     <a class="itemLink" href="/fr/c/analyse-financiere/">
+	 	       <span class="point"></span><span class="itemTitle">Analyse Financière</span>
+	 	     </a>
+	 	   </li>
+	 	   <li class="menuItem">
+	 	     <a class="itemLink" href="/fr/c/gestion-financiere/">
+	 	       <span class="point"></span><span class="itemTitle">Gestion des Finances</span>
+	 	     </a>
+	 	   </li>
+	 	   <li class="menuItem">
+	 	     <a class="itemLink" href="/fr/c/investissements/">
+	 	       <span class="point"></span><span class="itemTitle">Opportunités d’Investissement</span>
+	 	     </a>
+	 	   </li>`,
+	},
+	FOOTER_LEFT_MENU: {
+		
+	pt: `<a href="/pt/s/sobre-nos/">Quem Somos</a>
+		<div class="tabbarIcon"> /</div>
+		<a href="/pt/a/">Equipe Editorial</a>
+		<div class="tabbarIcon"> /</div>
+		<a href="/pt/s/contact/">Contato</a>
+		<div class="tabbarIcon"> /</div>
+		<a href="/pt/s/termos-de-uso/">Termos de Uso</a>
+		<div class="tabbarIcon"> /</div>
+		<a href="/pt/s/politica-de-privacidade/">Privacidade</a>`,
+
+	es: `<a href="/es/s/sobre-nosotros/">Quiénes Somos</a>
+		<div class="tabbarIcon"> /</div>
+		<a href="/es/a/">Equipo Editorial</a>
+		<div class="tabbarIcon"> /</div>
+		<a href="/es/s/contacto/">Contacto</a>
+		<div class="tabbarIcon"> /</div>
+		<a href="/es/s/condiciones-de-uso/">Condiciones de Uso</a>
+		<div class="tabbarIcon"> /</div>
+		<a href="/es/s/politica-de-privacidad/">Privacidad</a>`,
+		
+	en: `<a href="/s/about-us/">About Us</a>
+		<div class="tabbarIcon"> /</div>
+		<a href="/a/">Editorial Team</a>
+		<div class="tabbarIcon"> /</div>
+		<a href="/s/contact/">Contact</a>
+		<div class="tabbarIcon"> /</div>
+		<a href="/s/terms-of-use/">Terms of Use</a>
+		<div class="tabbarIcon"> /</div>
+		<a href="/s/privacy-policy/">Privacy</a>`,
+
+	de: `<a href="/de/s/uber-uns/">Über Uns</a>
+		<div class="tabbarIcon"> /</div>
+		<a href="/de/a/">Redaktionsteam</a>
+		<div class="tabbarIcon"> /</div>
+		<a href="/de/s/kontakt/">Kontakt</a>
+		<div class="tabbarIcon"> /</div>
+		<a href="/de/s/nutzungsbedingungen/">Nutzungsbedingungen</a>
+		<div class="tabbarIcon"> /</div>
+		<a href="/de/s/datenschutzerklarung/">Datenschutz</a>`,
+
+	fr: `<a href="/fr/s/a-propos/">À Propos</a>
+		<div class="tabbarIcon"> /</div>
+		<a href="/fr/a/">Équipe Éditoriale</a>
+		<div class="tabbarIcon"> /</div>
+		<a href="/fr/s/contact/">Contact</a>
+		<div class="tabbarIcon"> /</div>
+		<a href="/fr/s/conditions-d-utilisation/">Conditions d’Utilisation</a>
+		<div class="tabbarIcon"> /</div>
+		<a href="/fr/s/politique-de-confidentialite/">Confidentialité</a>`,
+	},
+	FOOTER_RIGHT_MENU: {
+	pt: `Aviso: O conteúdo deste site é apenas informativo e educativo, não constituindo orientação financeira profissional. Todo investimento envolve riscos e pode resultar em perdas parciais ou totais do capital. Esforçamo-nos para manter as informações precisas e atualizadas, mas não nos responsabilizamos por decisões tomadas com base neste material. Antes de investir, consulte sempre um consultor financeiro qualificado.`,
+	es: `Aviso: El contenido de este sitio es puramente informativo y educativo. No reemplaza la orientación financiera profesional. Toda inversión implica riesgos y puede generar pérdidas parciales o totales del capital. Nos esforzamos por mantener la información actualizada, pero no asumimos responsabilidad por decisiones basadas en este material. Se recomienda consultar con un asesor financiero calificado antes de invertir.`,
+	en: `Disclaimer: The content on this website is for informational and educational purposes only and does not constitute professional financial advice. All investments carry risks and may result in partial or total capital loss. While we strive to provide accurate and up-to-date information, we are not responsible for actions taken based on this material. Always seek guidance from a qualified financial advisor before investing.`,
+	de: `Haftungsausschluss: Die Inhalte dieser Website dienen ausschließlich Informations- und Bildungszwecken und stellen keine professionelle Finanzberatung dar. Jede Investition ist mit Risiken verbunden und kann zu teilweisem oder vollständigem Kapitalverlust führen. Wir bemühen uns, die Informationen aktuell zu halten, übernehmen jedoch keine Haftung für Entscheidungen, die auf diesen Inhalten beruhen. Lassen Sie sich vor einer Investition stets von einem qualifizierten Finanzberater beraten.`,
+	fr: `Avertissement : Le contenu de ce site est fourni uniquement à des fins d'information et d'éducation et ne constitue pas un conseil financier professionnel. Tout investissement comporte des risques et peut entraîner une perte partielle ou totale du capital. Nous nous efforçons de maintenir des informations exactes, mais déclinons toute responsabilité quant aux décisions prises sur cette base. Consultez toujours un conseiller financier qualifié avant d’investir.`,
+},
+
+	READ_TIME: {
+		en: 'minutes to read',
+		es: 'min de lectura',
+		pt: 'min de leitura',
+		fr: 'minutes de lecture',
+		de: 'Minuten zu lesen',
+	},
+	PRELOADER: {
+		LOADING: {
+			pt: 'Carregando',
+			en: 'Loading',
+			es: 'Cargando',
+			fr: 'Chargement',
+			de: 'Laden',
+		},
+		SECURE: {
+			pt: 'Site Seguro',
+			en: 'Secure Site',
+			es: 'Sitio Seguro',
+			fr: 'Site Sécurisé',
+			de: 'Sichere Seite',
+		},
+	},
+	WRITTEN_BY: {
+		en: 'By',
+		es: 'Por',
+		pt: 'Por',
+		fr: 'Par',
+		de: 'Von',
+	},
+	POPULAR_CATEGORIES_TITLE: {
+		en: 'Popular Categories',
+		es: 'Categorías Populares',
+		pt: 'Categorias Populares',
+		fr: 'Catégories Populaires',
+		de: 'Beliebte Kategorien',
+	},
+	LOGO_URL: 'https://via.placeholder.com/150',
+	ALL_RIGHTS: {
+		en: `All rights reserved.`,
+		pt: `Todos os direitos reservados.`,
+		es: `Todos los derechos reservados.`,
+		fr: `Tous droits réservés.`,
+		de: `Alle Rechte vorbehalten.`,
+	},
+	SOCIAL_MEDIA: {
+		instagram: 'https://www.instagram.com/mejorescreditos/',
+		facebook: 'https://www.facebook.com/mejorescreditos/',
+	},
+	PRO_POINTS_LANGUAGE: {
+		pt: 'Pontos Positivos',
+		es: 'Puntos Positivos',
+		en: 'Pros',
+		fr: 'Points Positifs',
+		de: 'Vorteile',
+	},
+	CONS_POINTS_LANGUAGE: {
+		pt: 'Pontos Negativos',
+		es: 'Puntos Negativos',
+		en: 'Cons',
+		fr: 'Points Négatifs',
+		de: 'Nachteile',
+	},
+	SIDEBAR_TITLE: {
+		pt: 'Últimos Posts',
+		en: 'Latest Articles',
+		es: 'Últimos Artículos',
+		fr: 'Derniers Articles',
+		de: 'Neueste Artikel',
+	},
+	RELATED_TITLE: {
+		pt: 'Artigos Relacionados',
+		en: 'Related Articles',
+		es: 'Artículos Relacionados',
+		fr: 'Articles Connexes',
+		de: 'Verwandte Artikel',
+	},
+	CONTACT_DATA: {
+		TITLE: {
+			pt: 'Contato',
+			en: 'Contact',
+			es: 'Contacto',
+			fr: 'Contact',
+			de: 'Kontakt',
+		},
+		SUBTITLE: {
+			pt: 'Entre em Contato com Nossa Equipe',
+			en: 'Get in Contact with Our Team',
+			es: 'Póngase en Contacto con Nuestro Equipo',
+			fr: 'Contactez Notre Équipe',
+			de: 'Nehmen Sie Kontakt mit Unserem Team auf',
+		},
+		TEXT: {
+			pt: 'Nos envie uma mensagem usando o formulário abaixo e nós entraremos em contato o mais rápido possível.',
+			en: 'Send us a message using the form below and we will contact you as soon as possible.',
+			es: 'Envíenos un mensaje mediante el siguiente formulario y nos pondremos en contacto con usted lo antes posible.',
+			fr: 'Envoyez-nous un message via le formulaire ci-dessous et nous vous contacterons dès que possible.',
+			de: 'Senden Sie uns eine Nachricht über das untenstehende Formular und wir werden Sie so schnell wie möglich kontaktieren.',
+		},
+		PLACEHOLDER_NAME: {
+			pt: 'Nome',
+			en: 'Name',
+			es: 'Nombre',
+			fr: 'Nom',
+			de: 'Name',
+		},
+		PLACEHOLDER_SUBJECT: {
+			pt: 'Assunto',
+			en: 'Subject',
+			es: 'Asunto',
+			fr: 'Sujet',
+			de: 'Betreff',
+		},
+		PLACEHOLDER_MESSAGE: {
+			pt: 'Mensagem',
+			en: 'Message',
+			es: 'Mensaje',
+			fr: 'Message',
+			de: 'Nachricht',
+		},
+		BUTTON_TEXT: {
+			pt: 'Enviar Mensagem',
+			en: 'Send Message',
+			es: 'Enviar Mensaje',
+			fr: 'Envoyer le Message',
+			de: 'Nachricht Senden',
+		},
+		SENDING_MESSAGE: {
+			pt: 'Enviando...',
+			en: 'Sending...',
+			es: 'Envío...',
+			fr: 'Envoi...',
+			de: 'Senden...',
+		},
+		SUCCESS_MESSAGE: {
+			pt: 'Sua mensagem foi enviada com sucesso!',
+			en: 'Your message has been sent successfully!',
+			es: '¡Tu mensaje ha sido enviado exitosamente!',
+			fr: 'Votre message a été envoyé avec succès !',
+			de: 'Ihre Nachricht wurde erfolgreich gesendet!',
+		},
+		SERVER_ERROR_MESSAGE: {
+			pt: 'Ocorreu um erro ao mandar sua mensagem.',
+			en: 'An error occurred while sending your message.',
+			es: 'Se produjo un error al enviar su mensaje.',
+			fr: `Une erreur s'est produite lors de l'envoi de votre message.`,
+			de: 'Beim Senden Ihrer Nachricht ist ein Fehler aufgetreten.',
+		},
+		FORM_ERROR_MESSAGE: {
+			pt: 'Houve um problema ao mandar sua mensagem. Por favor tente novamente mais tarde.',
+			en: 'There was a problem submitting your message. Please try again later.',
+			es: 'Hubo un problema al enviar su mensaje. Inténtelo de nuevo más tarde.',
+			fr: `Un problème est survenu lors de l'envoi de votre message. Veuillez réessayer plus tard.`,
+			de: 'Beim Senden Ihrer Nachricht ist ein Problem aufgetreten. Bitte versuchen Sie es später erneut.',
+		},
+		FILL_INPUTS_MESSAGE: {
+			pt: 'Preencha todos os campos',
+			en: 'Please fill in all fields',
+			es: 'Por favor, rellena todos los campos',
+			fr: 'Veuillez remplir tous les champs',
+			de: 'Bitte füllen Sie alle Felder aus',
+		},
+	},
+	PAGINATION: {
+		NEXT: {
+			pt: 'Próxima',
+			en: 'Next',
+			es: 'Seguinte',
+			fr: 'Suivant',
+			de: 'Nächste',
+		},
+		PREVIOUS: {
+			pt: 'Anterior',
+			en: 'Previous',
+			es: 'Anterior',
+			fr: 'Précédent',
+			de: 'Vorherige',
+		},
+	},
+	DATE_FORMATS: {
+		en: 'MM/DD/YYYY',
+		es: 'DD/MM/YYYY',
+		pt: 'DD/MM/YYYY - HH:mm',
+		de: 'DD.MM.YYYY',
+		fr: 'DD/MM/YYYY',
+	},
+	AUTHORS_LIST_TITLE: {
+		en: 'Authors',
+		es: 'Autores',
+		pt: 'Conheça Nosso Time',
+		de: 'Autoren',
+		fr: 'Auteurs',
+	},
+	AUTHOR_ABOUT_TITLE: {
+		pt: 'Sobre o Autor:',
+		en: 'About the Author:',
+		es: 'Sobre el Autor:',
+		fr: "À propos de l'auteur:",
+		de: 'Über den Autor:',
+	},
+	NOT_FOUND: {
+		pt: {
+			TITLE: 'Página não encontrada',
+			TEXT: 'A URL pode estar incorreta ou a página pode não existir mais.',
+		},
+		en: {
+			TITLE: 'Page Not Found',
+			TEXT: 'The URL might be incorrect or the page may no longer exist.',
+		},
+		es: {
+			TITLE: 'Página no encontrada',
+			TEXT: 'Puede que la URL sea incorrecta o que la página ya no exista.',
+		},
+		de: {
+			TITLE: 'Seite nicht gefunden',
+			TEXT: 'Die URL könnte falsch sein oder die Seite existiert nicht mehr.',
+		},
+		fr: {
+			TITLE: 'Page non trouvée',
+			TEXT: "L'URL est peut-être incorrecte ou la page n'existe plus.",
+		},
+	},
+	READ_ALSO: {
+		pt: 'Leia também...',
+		en: 'Read also...',
+		es: 'Lee también...',
+		de: 'Lies auch...',
+		fr: 'Lire aussi...',
+	},
+	SEARCH: {
+		NOT_FOUND: {
+			pt: 'Nenhum post encontrado.',
+			en: 'No posts found.',
+			es: 'No se encontraron publicaciones.',
+			de: 'Keine Beiträge gefunden.',
+			fr: 'Aucun post trouvé.',
+		},
+		RESULT: {
+			pt: 'Pesquisa',
+			en: 'Search',
+			es: 'Buscar',
+			de: 'Suche',
+			fr: 'Recherche',
+		},
+		ACTION: {
+			pt: 'Pesquisar',
+			en: 'Search',
+			es: 'Buscar',
+			de: 'Suche',
+			fr: 'Recherche',
+		},
+		PLACEHOLDER: {
+			pt: 'Buscar',
+			en: 'Search',
+			es: 'Buscar',
+			fr: 'Rechercher',
+			de: 'Suche',
+		},
+	},
+	READ_MORE_TITLE: {
+		pt: 'Leia mais',
+		en: 'Read more',
+		es: 'Leer más',
+		fr: 'Lire la suite',
+		de: 'Weiterlesen',
+	},
+	POSTS_BY: {
+		pt: 'Posts de',
+		en: 'Posts by',
+		es: 'Posts de',
+		fr: 'Posts de',
+		de: 'Beiträge von',
+	},
+	CATEGORY: {
+		pt: 'Categoria:',
+		en: 'Category:',
+		es: 'Categoría:',
+		fr: 'Catégorie:',
+		de: 'Kategorie:',
+	},
+	REFERENCES_TITLE: {
+		pt: 'Referências',
+		en: 'References',
+		es: 'Referencias',
+		fr: 'Références',
+		de: 'Referenzen',
+	},
+	TABLE_OF_CONTENT_TITLE: {
+		en: 'Table of Contents',
+		es: 'Tabla de Contenidos',
+		pt: 'Tabela de Conteúdo',
+		de: 'Inhaltsverzeichnis',
+		fr: 'Table des Matières',
+	},
+});
